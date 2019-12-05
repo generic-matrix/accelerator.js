d4bd5e21db9076c5ba889f1df0e7bf42e25edff1
kaustubhb <kaustubhbawa123@gmail.com>
2019-12-05 19:47:36 +0530

    Update test.js

--- test.js
+++ test.js
@@ -4,35 +4,6 @@ const now = require('nano-time');
 var init_time=now();
 var settings=
     {
-        "use_lib":"tf",
-    };
-var acc=new Accelerator.accelerator(settings);
-var util=new Accelerator.util(settings);
-var t1=acc.define_array([10,20,0.30,0.40]);
-var t2=acc.define_array([10,20,0.30,0.40]);
-
-//var linear_t=util.linear_mul(t1,t2);
-//var pow_t=util.pow(t1,t2);
-//var exp_t=util.exp(t1);
-for (var i = 0; i < 20000; i++) {
-    var add_t=util.add(t1,t2);
-    var sub_t=util.sub(t1,t2);
-    acc.get_array(add_t);
-    acc.get_array(sub_t);
-}
-//console.log(acc.get_array(linear_t));
-//console.log(acc.get_array(pow_t));
-//console.log(acc.get_array(exp_t));
-
-var final_time=now();
-console.log("Time taken GPU : "+(final_time-init_time)/1000000000+" sec");
-
-
-console.log("\n ------------------------------------ \n");
-
-init_time=now();
-var settings=
-    {
         "use_lib":"tf-cuda",
     };
 var acc=new Accelerator.accelerator(settings);
@@ -45,17 +16,46 @@ var t2=acc.define_array([10,20,0.30,0.40]);
 //var pow_t=util.pow(t1,t2);
 //var exp_t=util.exp(t1);
 for (var i = 0; i < 20000; i++) {
-    var add_t=util.add(t1,t2);
-    var sub_t=util.sub(t1,t2);
-    acc.get_array(add_t);
-    acc.get_array(sub_t);
+  var add_t=util.add(t1,t2);
+  var sub_t=util.sub(t1,t2);
+  acc.get_array(add_t);
+  acc.get_array(sub_t);
 }
 //console.log(acc.get_array(linear_t));
 //console.log(acc.get_array(pow_t));
 //console.log(acc.get_array(exp_t));
 
-final_time=now();
+var final_time=now();
 console.log("Time taken CUDA : "+(final_time-init_time)/1000000000+" sec");
+
+console.log("\n ------------------------------------ \n");
+
+var init_time=now();
+var settings=
+    {
+        "use_lib":"tf",
+    };
+var acc=new Accelerator.accelerator(settings);
+var util=new Accelerator.util(settings);
+var t1=acc.define_array([10,20,0.30,0.40]);
+var t2=acc.define_array([10,20,0.30,0.40]);
+
+//var linear_t=util.linear_mul(t1,t2);
+//var pow_t=util.pow(t1,t2);
+//var exp_t=util.exp(t1);
+for (var i = 0; i < 20000; i++) {
+  var add_t=util.add(t1,t2);
+  var sub_t=util.sub(t1,t2);
+  acc.get_array(add_t);
+  acc.get_array(sub_t);
+}
+//console.log(acc.get_array(linear_t));
+//console.log(acc.get_array(pow_t));
+//console.log(acc.get_array(exp_t));
+
+var final_time=now();
+console.log("Time taken GPU : "+(final_time-init_time)/1000000000+" sec");
+
 
 console.log("\n ------------------------------------ \n");
 
