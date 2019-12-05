c6872fced63f864553aafff1a84c4cf15d119afb
kaustubhb <kaustubhbawa123@gmail.com>
2019-12-05 20:21:21 +0530

    Removed arraySync

--- src/Util.js
+++ src/Util.js
@@ -4,23 +4,23 @@ class Util{
         this.acc=new Accelerator(json);
     }
     add(vec1,vec2){
-        return this.acc.define_array(vec1[vec1.var_name].add(vec2[vec2.var_name]).arraySync());
+        return this.acc.define_array(vec1[vec1.var_name].add(vec2[vec2.var_name]));
     }
 
     sub(vec1,vec2){
-        return this.acc.define_array(vec1[vec1.var_name].sub(vec2[vec2.var_name]).arraySync());
+        return this.acc.define_array(vec1[vec1.var_name].sub(vec2[vec2.var_name]));
     }
 
     linear_mul(vec1,vec2){
-        return this.acc.define_array(vec1[vec1.var_name].mul(vec2[vec2.var_name]).arraySync());
+        return this.acc.define_array(vec1[vec1.var_name].mul(vec2[vec2.var_name]));
     }
 
     pow(vec1,vec2){
-        return this.acc.define_array(vec1[vec1.var_name].pow(vec2[vec2.var_name]).arraySync());
+        return this.acc.define_array(vec1[vec1.var_name].pow(vec2[vec2.var_name]));
     }
 
     exp(vec1){
-        return this.acc.define_array(vec1[vec1.var_name].exp().arraySync());
+        return this.acc.define_array(vec1[vec1.var_name].exp());
     }
 
 }
