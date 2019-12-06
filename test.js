1af586ff00c10c0cb16f36b0762283fc4d0c042e
kaustubhb <kaustubhbawa123@gmail.com>
2019-12-06 11:04:32 +0530

    Update test.js

--- test.js
+++ test.js
@@ -12,18 +12,18 @@ var t1=acc.define_array(new Array(50).fill(0.8));
 var t2=acc.define_array(new Array(50).fill(123.8));
 
 
-var linear_t=util.linear_mul(t1,t2);
-var pow_t=util.pow(t1,t2);
-var exp_t=util.exp(t1);
+//var linear_t=util.linear_mul(t1,t2);
+//var pow_t=util.pow(t1,t2);
+//var exp_t=util.exp(t1);
 for (var i = 0; i < 200; i++) {
   var add_t=util.add(t1,t2);
   var sub_t=util.sub(t1,t2);
   //acc.get_array(add_t);
   //acc.get_array(sub_t);
 }
-console.log(acc.get_array(linear_t));
-console.log(acc.get_array(pow_t));
-console.log(acc.get_array(exp_t));
+//console.log(acc.get_array(linear_t));
+//console.log(acc.get_array(pow_t));
+//console.log(acc.get_array(exp_t));
 
 var final_time=now();
 console.log("Time taken CUDA : "+(final_time-init_time)/1000000000+" sec");
@@ -40,18 +40,18 @@ var util=new Accelerator.util(settings);
 var t1=acc.define_array(new Array(50).fill(0.8));
 var t2=acc.define_array(new Array(50).fill(123.8));
 
-var linear_t=util.linear_mul(t1,t2);
-var pow_t=util.pow(t1,t2);
-var exp_t=util.exp(t1);
+//var linear_t=util.linear_mul(t1,t2);
+//var pow_t=util.pow(t1,t2);
+//var exp_t=util.exp(t1);
 for (var i = 0; i < 200; i++) {
   var add_t=util.add(t1,t2);
   var sub_t=util.sub(t1,t2);
   //acc.get_array(add_t);
   //acc.get_array(sub_t);
 }
-console.log(acc.get_array(linear_t));
-console.log(acc.get_array(pow_t));
-console.log(acc.get_array(exp_t));
+//console.log(acc.get_array(linear_t));
+//console.log(acc.get_array(pow_t));
+//console.log(acc.get_array(exp_t));
 
 var final_time=now();
 console.log("Time taken GPU : "+(final_time-init_time)/1000000000+" sec");
