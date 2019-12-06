c58433fdad157cb6938dd86695914c8c95341284
kaustubhb <kaustubhbawa123@gmail.com>
2019-12-06 23:12:17 +0530

    Updated for generics.js

--- src/Util.js
+++ src/Util.js
@@ -4,39 +4,39 @@ class Util{
         this.acc=new Accelerator(json);
     }
     add(vec1,vec2){
-        return this.acc.define_array(vec1[vec1.var_name].add(vec2[vec2.var_name]).arraySync());
+        return this.acc.copy_obj(vec1[vec1.var_name].add(vec2[vec2.var_name]));
     }
 
     sub(vec1,vec2){
-        return this.acc.define_array(vec1[vec1.var_name].sub(vec2[vec2.var_name]).arraySync());
+        return this.acc.copy_obj(vec1[vec1.var_name].sub(vec2[vec2.var_name]));
     }
 
     linear_mul(vec1,vec2){
-        return this.acc.define_array(vec1[vec1.var_name].mul(vec2[vec2.var_name]).arraySync());
+        return this.acc.copy_obj(vec1[vec1.var_name].mul(vec2[vec2.var_name]));
     }
-    
+
     linear_div(vec1,vec2){
-        return this.acc.define_array(vec1[vec1.var_name].div(vec2[vec2.var_name]).arraySync());
+        return this.acc.copy_obj(vec1[vec1.var_name].div(vec2[vec2.var_name]));
     }
-    
+
     linear_max(vec1,vec2){
         if(vec1>vec2){
             return vec1;
         }else{
             return vec2;
-        }    
+        }
     }
-    
+
     linear_max_boolean(vec1,vec2){
         return vec1>vec2;
     }
 
     pow(vec1,vec2){
-        return this.acc.define_array(vec1[vec1.var_name].pow(vec2[vec2.var_name]).arraySync());
+        return this.acc.copy_obj(vec1[vec1.var_name].pow(vec2[vec2.var_name]));
     }
 
     exp(vec1){
-        return this.acc.define_array(vec1[vec1.var_name].exp().arraySync());
+        return this.acc.copy_obj(vec1[vec1.var_name].exp());
     }
 
 }
