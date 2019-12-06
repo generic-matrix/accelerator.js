b2c662e2b33a0e280cefccf8ba9a4c967f868c9c
kaustubhb <kaustubhbawa123@gmail.com>
2019-12-06 23:11:04 +0530

    Updated for generics.js

--- src/Accelerator.js
+++ src/Accelerator.js
@@ -1,4 +1,5 @@
 var binder=require("./Binding");
+const tf = require("@tensorflow/tfjs-node");
 class Accelerator{
     /*
     JSON -->
@@ -7,23 +8,43 @@ class Accelerator{
         }
      */
     constructor(json) {
+        this.settings=json;
         if(!json.hasOwnProperty("use_lib")){
                 throw new Error("The JSON is invalid ,do refer the docs.");
         }else if(json["use_lib"]==="tf"){
             console.log("Using Tensorflow (tfjs-node) core as backend .");
+            this.zero_arr=this.define_array([0]);
+            this.one_arr=this.define_array([1]);
+            this.two_arr=this.define_array([2]);
+            this.minus_one_arr=this.define_array([-1]);
+            this.zero_one=this.define_array([0.1]);
+            this.zero_zero_one=this.define_array([0.01]);
+            this.zero_zero_zero_one=this.define_array([0.001]);
         }else if(json["use_lib"]==="tf-cuda"){
             console.log("Using Tensorflow (tfjs cuda) core as backend .");
+            const tf_cuda = require("@tensorflow/tfjs-node-gpu");
+            this.zero_arr=this.define_array([0]);
+            this.one_arr=this.define_array([1]);
+            this.two_arr=this.define_array([2]);
+            this.minus_one_arr=this.define_array([-1]);
+            this.zero_one=this.define_array([0.1]);
+            this.zero_zero_one=this.define_array([0.01]);
+            this.zero_zero_zero_one=this.define_array([0.001]);
         }else{
             throw new Error("invalid option specified.");
         }
-        this.settings=json;
     }
+
 
     /*
         A function to load a array to GPU's address space using the library .
      */
     define_array(array){
         return new binder(array,this.settings);
+    }
+
+    copy_obj(obj){
+        return new binder(obj,"copy");
     }
 
     get_array(elem){
